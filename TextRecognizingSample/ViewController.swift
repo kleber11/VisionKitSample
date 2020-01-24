8d66095140e9419a2e4b0955b08231a0e262b89e
Vladyslav Vcherashnii <vladyslav.vcherashnii@gmail.com>
2020-01-23 21:58:20 -0500

    Initial Commit

---
+++ TextRecognizingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognizingSample
+//
+//  Created by Vladyslav Vcherashnii on 23.01.2020.
+//  Copyright © 2020 Vladyslav Vcherashnii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
